3e51e78ec03fc74a9fd8adf7ca933a42f3ed4a68
Mukeshkumar0202 <webfort.mukeshkumar@gmail.com>
2022-09-30 18:09:31 +0530

    updates

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,22 @@
 <template>
-    <div>
-        kishan
+    <div class="py-16 bg-slate-100">
+        <div class="container mx-auto px-12 grid grid-cols-2 gap-5">
+            <!-- Text Panel -->
+            <div>
+                <div class="w-80 space-y-4">
+                    <div class="text-3xl">Surface Laptop 4</div>
+                    <div>Do it all with a perfect balance of sleek, ultra-thin design, multitasking speed and improved
+                        performance</div>
+
+                    <button type="button"
+                        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Shop now</button>
+                </div>
+            </div>
+
+            <!-- Image Panel -->
+            <div class="">
+                <img src="src/assets/nordwood-themes-kRNZiGKtz48-unsplash.jpg" alt="">
+            </div>
+        </div>
     </div>
 </template>
